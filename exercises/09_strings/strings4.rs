14f7e63f482cf076ca11ef3187c0a7173f03bc3f
linjinzhen <2053268305@qq.com>
2024-12-06 14:18:15 +0800

    字符串练习

--- exercises/09_strings/strings4.rs
+++ exercises/09_strings/strings4.rs
@@ -1,12 +1,36 @@
 // Calls of this function should be replaced with calls of `string_slice` or `string`.
-fn placeholder() {}
+// 定义一个 trait，用于处理字符串
+trait StringHandler {
+    fn process(&self);
+}
+
+// 实现 trait 用于 &str 类型
+impl StringHandler for &str {
+    fn process(&self) {
+        string_slice(self);
+        // println!("Processing &str: {}", self);
+    }
+}
+
+// 实现 trait 用于 String 类型
+impl StringHandler for String {
+    fn process(&self) {
+        string(self.clone());
+        //println!("Processing String: {}", self);
+    }
+}
+
+// 泛型函数，用于接受实现了 StringHandler trait 的类型
+fn placeholder<T: StringHandler>(arg: T) {
+    arg.process(); // 根据 arg 实现的 process 方法进行处理
+}
 
 fn string_slice(arg: &str) {
-    println!("{arg}");
+    println!("&str {arg}");
 }
 
 fn string(arg: String) {
-    println!("{arg}");
+    println!("String {arg}");
 }
 
 // TODO: Here are a bunch of values - some are `String`, some are `&str`.
@@ -21,7 +45,7 @@ fn main() {
 
     placeholder("rust is fun!".to_owned());
 
-    placeholder("nice weather".into());
+    placeholder::<&str>("nice weather".into());
 
     placeholder(format!("Interpolation {}", "Station"));
 
