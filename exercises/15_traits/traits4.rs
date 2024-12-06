ee496aae22cb5da4254ccf6f69b74a631e0b4a0a
linjinzhen <2053268305@qq.com>
2024-12-06 17:48:35 +0800

    特征通关

--- exercises/15_traits/traits4.rs
+++ exercises/15_traits/traits4.rs
@@ -7,11 +7,19 @@ trait Licensed {
 struct SomeSoftware;
 struct OtherSoftware;
 
-impl Licensed for SomeSoftware {}
-impl Licensed for OtherSoftware {}
+impl Licensed for SomeSoftware {
+    fn licensing_info(&self) -> String {
+        "Default license".to_string()
+    }
+}
+impl Licensed for OtherSoftware {
+    fn licensing_info(&self) -> String {
+        "Default license".to_string()
+    }
+}
 
 // TODO: Fix the compiler error by only changing the signature of this function.
-fn compare_license_types(software1: ???, software2: ???) -> bool {
+fn compare_license_types<T1:Licensed,T2:Licensed>(software1: T1, software2: T2) -> bool {
     software1.licensing_info() == software2.licensing_info()
 }
 
